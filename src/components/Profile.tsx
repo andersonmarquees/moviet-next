b9831195b9c43a734244ffb2e8f99f1fd2f81e75
Anderson Marques <anderson_fisico@yahoo.com.br>
2021-06-20 06:27:51 -0300

    Component Challenge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
       <img src="https://github.com/andersonmarquees.png" alt="avatar" />
       <div>
         <strong>Anderson Marques</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+        </p>
       </div>
     </div>
   );
